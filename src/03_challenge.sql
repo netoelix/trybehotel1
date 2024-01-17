cf13736ade79b61d738b0bd4486b8fc88e4e0935
Elix Neto <neto.elix@outlook.com>
2024-01-17 15:08:36 -0300

    Finalizando requisito 3

---
+++ src/03_challenge.sql
@@ -0,0 +1,8 @@
+CREATE TABLE trybe_hotel.users (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(60),
+    age INTEGER,
+    created_at DATE DEFAULT(NOW())
+);
